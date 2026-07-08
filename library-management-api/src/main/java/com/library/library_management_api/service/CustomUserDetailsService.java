22fe55bd9e4c85fc6a13515a3e005097464e1917
NChaithra9 <n.chaithra@calfus.com>
2026-07-08 12:08:30 +0530

    Custom User details

---
+++ library-management-api/src/main/java/com/library/library_management_api/service/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.library.library_management_api.service;
+
+import com.library.library_management_api.entity.User;
+import com.library.library_management_api.repository.UserRepository;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getRole())
+                .build();
+    }
+}
